a7b7035937ed944aaed949dd89264a30d9bc84f0
HuseynovRuslan <155317262+HuseynovRuslan@users.noreply.github.com>
2025-08-30 15:38:19 +0400

    create-mvc-app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Pixel-ecommerce
+
+## Dependencies
+
+This project depends on the following NuGet packages:
+
+* `Microsoft.EntityFrameworkCore` (9.0.8)
+* `Microsoft.EntityFrameworkCore.Design` (9.0.8)
+* `Microsoft.EntityFrameworkCore.SqlServer` (9.0.8)
+* `Microsoft.EntityFrameworkCore.Tools` (9.0.8)
---
+++ pixel-ecommerce-backend/Pixel.WebUI/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Pixel.WebUI.Models.Persistences;
+
+namespace Pixel.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<DataContext>( cfg =>
+            {
+                cfg.UseSqlServer(builder.Configuration.GetConnectionString("cString"));
+            });
+
+
+
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+
+
+
+            });
+
+
+            app.Run();
+        }
+    }
+}
